f61beaeeac63e04fb8505805d108f5fd3b9d9cc4
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2018-02-12 08:31:30 +0100

    WIP

---
+++ compile.sh
@@ -0,0 +1,1 @@
+solc github.com/OpenZeppelin/zeppelin-solidity/=/Users/marek/Documents/blockchain/planar/ticket-contract/node_modules/zeppelin-solidity/ --bin src/TravelTicket.sol
--- src/TravelTicket.sol
+++ src/TravelTicket.sol
@@ -1,7 +1,13 @@
 pragma solidity ^0.4.18;
 
-import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+import 'zeppelin/contracts/token/ERC721/ERC721Token.sol';
 
 contract TravelTicket is ERC721Token {
-  
+
+  mapping (address => string) public addressValue;
+
+  function TravelTicket() public {
+
+  }
+
 }
