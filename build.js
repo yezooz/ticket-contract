1743efd9bc46c2fbec775f93dc5759443474eb15
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2018-02-12 12:45:18 +0100

    deploy with web3

--- build.js
+++ build.js
@@ -17,21 +17,40 @@ function findImports(path) {
 const compiledCode = solc.compile({
   'sources': inputs
 }, 1, findImports);
-console.log(compiledCode);
+// console.log(compiledCode);
 
 if (compiledCode.errors && compiledCode.errors.length > 0) {
   console.error(compiledCode.errors);
+  return;
 }
 
-for (const id in compiledCode.contracts) {
-  console.log(id);
-  console.log(compiledCode.contracts[id].interface);
-}
+fs.writeFile('./build/TravelTicket.json', JSON.stringify(compiledCode), function(err) {
+  if (err) 
+    throw err;
+  console.log('Compilation complete');
+});
 
-const TravelTicketABI = compiledCode.contracts['TravelTicket.sol:TravelTicket'].interface;
-console.log(TravelTicketABI);
+// for (const id in compiledCode.contracts) {
+//   console.log(id);
+//   console.log(compiledCode.contracts[id].interface);
+// }
 
-// fs.writeFile('./build/TravelTicket.json', JSON.stringify(compiledCode), function(err) {
-//     if (err) throw err;
-//     console.log('Compilation complete');
-// });
+const travelTicket = compiledCode.contracts['TravelTicket.sol:TravelTicket'];
+console.log(travelTicket);
+
+const Web3 = require('web3');
+const web3 = new Web3();
+web3.setProvider(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+const abi = JSON.parse(travelTicket.interface);
+const bytecode = travelTicket.bytecode;
+const travelTicketInstance = new web3.eth.Contract(abi, {
+  data: '0x' + bytecode,
+  gas: 90000 * 2
+});
+
+web3.eth.getAccounts().then(accounts => {
+  return travelTicketInstance.deploy().send({from: accounts[0], gas: 4712388, gasPrice: '100000000000'});
+}).then((receipt) => {
+  console.log(`Contract deployed @ ${receipt.options.address}`);
+});
