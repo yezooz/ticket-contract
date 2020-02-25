7e512aadfc51579719ee0d2b09f4c1c51a1d3656
Marek Mikuliszyn <marek.mikuliszyn@gmail.com>
2020-02-25 09:52:41 +0000

    Solc (#42)
    * Ignore .idea
    
    * WIP

--- src/TravelTicket.sol
+++ src/TravelTicket.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.19;
 
-import 'zeppelin-solidity/contracts/token/ERC721/ERC721Token.sol';
+import 'zeppelin/contracts/token/ERC721/ERC721Token.sol';
 
 contract TravelTicket is ERC721Token {
+  mapping (address => string) public addressValue;
 
+  function TravelTicket() public {
+
+  }
 }
